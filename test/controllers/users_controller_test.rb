0398d4bd7949b00904c3ac5c1ca3eeaba5179919
Jorge Sánchez <jorge@questionity.com>
2015-10-10 12:13:09 +0200

    refactor "magic numbers" in the UsersControllerTest #23

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  USERNAME_VALID_FORMAT_NOT_REGISTERED = USERNAME_VALID_FORMAT_NOT_REGISTERED
+  USERNAME_VALID_FORMAT_NOT_REGISTERED = 'imnotregistered'
   EMAIL_VALID_FORMAT_NOT_REGISTERED = 'thisemailsisnotregistered@themonkeyisland.com'
   PASSWORD_VALID_FORMAT_STANDARD_FOR_ALL_USERS = 'secret'
   PASSWORD_VALID_FORMAT_STANDARD_FOR_UPDATE = 'new_password'
