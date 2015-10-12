3fbf446ce4d59664a3645f4c8f931a77a4fd8711
Jorge Sánchez <jorge@questionity.com>
2015-10-12 12:06:55 +0200

    Refactor "magic numbers" for a wrong password #27

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionController::TestCase
   USERNAME_VALID_FORMAT_NOT_REGISTERED = 'imnotregistered'
   EMAIL_VALID_FORMAT_NOT_REGISTERED = 'thisemailsisnotregistered@themonkeyisland.com'
   PASSWORD_VALID_FORMAT_STANDARD_FOR_ALL_USERS = 'secret'
+  PASSWORD_VALID_FORMAT_WRONG = 'wrong'
   PASSWORD_VALID_FORMAT_STANDARD_FOR_UPDATE = 'new_password'
 
 
