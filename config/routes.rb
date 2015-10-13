7a98410c6c3284c3bae43e0bdb3e2411ff843112
Jorge Sánchez <jorge@questionity.com>
2015-10-13 16:49:50 +0200

    Create scaffold for the products item #45

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   # Root page
   root 'site#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005101237) do
+ActiveRecord::Schema.define(version: 20151013144842) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",          limit: 15
