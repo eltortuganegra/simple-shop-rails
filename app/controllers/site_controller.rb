51ab1672ace281d4a2ef8a207ec42397cdd5f83f
Jorge Sánchez <jorge@questionity.com>
2016-01-18 08:38:01 +0100

    Refactored site controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,28 +2,4 @@ class SiteController < ApplicationController
   def index
   end
 
-  def about
-  end
-
-  def terms_of_service
-  end
-
-  def privacy
-  end
-
-  def cookies_page
-  end
-
-  def contact
-  end
-
-  def faq
-  end
-
-  def contact
-  end
-
-  def press
-  end
-
 end
--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -6,29 +6,4 @@ class SiteControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get about" do
-    get :about
-    assert_response :success
-  end
-
-  test "should get terms" do
-    get :terms_of_service
-    assert_response :success
-  end
-
-  test "should get privacy" do
-    get :privacy
-    assert_response :success
-  end
-
-  test "should get cookies" do
-    get :cookies_page
-    assert_response :success
-  end
-
-  test "should get contact" do
-    get :contact
-    assert_response :success
-  end
-
 end
