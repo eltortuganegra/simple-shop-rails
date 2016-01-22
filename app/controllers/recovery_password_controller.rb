a2f5e785b4dbb4ac8796719f1f3f3d122f3f62d4
Jorge Sánchez <jorge@questionity.com>
2016-01-22 09:17:04 +0100

    When users send the username or email for recovery the password without data they are redirect to /recovery_password #101

--- app/controllers/recovery_password_controller.rb
+++ app/controllers/recovery_password_controller.rb
@@ -4,7 +4,7 @@ class RecoveryPasswordController < ApplicationController
 
   def confirm_code
     if ! recovery_password_params.has_key? :username_or_email
-      redirect_to recovery_password_path
+      redirect_to recovery_password_path, notice: 'Username or email is not found.'
     end
   end
 
@@ -12,7 +12,6 @@ class RecoveryPasswordController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def recovery_password_params
       params.fetch(:recovery_password, {}).permit(:username_or_email)
-      # params
     end
 
 end
