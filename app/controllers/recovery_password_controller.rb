e8f5afbd5ee967e93b106acbfc035b6751e585e8
eltortuganegra <contacto@eltortuganegra.com>
2016-01-24 20:47:35 +0100

    Refactored parameters

--- app/controllers/recovery_password_controller.rb
+++ app/controllers/recovery_password_controller.rb
@@ -3,8 +3,10 @@ class RecoveryPasswordController < ApplicationController
   end
 
   def confirm_code
-    if ! recovery_password_params.has_key? :username_or_email
-      redirect_to recovery_password_path, notice: 'Username or email is not found.'
+    if is_username_or_email_parameter_valid?
+
+    else
+      redirect_to recovery_password_path, notice: 'Username or email is not found.' + params.inspect
     end
   end
 
@@ -12,6 +14,18 @@ class RecoveryPasswordController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def recovery_password_params
       params.fetch(:recovery_password, {}).permit(:username_or_email)
+    end
+
+    def is_username_or_email_parameter_valid?
+        does_username_or_email_parameter_exist? && ! is_username_or_email_parameter_void?
+    end
+
+    def does_username_or_email_parameter_exist?
+        recovery_password_params.has_key? :username_or_email
+    end
+
+    def is_username_or_email_parameter_void?
+      recovery_password_params[:username_or_email] == ''
     end
 
 end
--- test/controllers/recovery_password_controller_test.rb
+++ test/controllers/recovery_password_controller_test.rb
@@ -11,13 +11,13 @@ class RecoveryPasswordControllerTest < ActionController::TestCase
     assert_select 'form', 1, 'Form with name "recovery_password_form" not found.'
   end
 
-  test "should exist a field with name 'recovery_password[email_or_username]' in the form with name 'recovery_password_form'" do
+  test "should exist a field with name 'recovery_password[username_or_email]' in the form with name 'recovery_password_form'" do
     get :index
-    assert_select 'form[name="recovery_password_form"] input[name="recovery_password[email_or_username]"]', 1, 'Input with name "email_or_username" not found.'
+    assert_select 'form[name="recovery_password_form"] input[name="recovery_password[username_or_email]"]', 1, 'Input with name "email_or_username" not found.'
   end
 
   test "When users send the username or email for recovery the password without data they are redirect to /recovery_password" do
-    post :confirm_code 
+    post :confirm_code
     assert_redirected_to recovery_password_path
   end
 
