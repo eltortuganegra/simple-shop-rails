6990c673b1b1919a91fa0c90058c40d94a99aebb
eltortuganegra <jorge@questionity.com>
2016-04-19 09:20:08 +0200

    When users are logged must can access to their setting page through the toolbar #114

--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
+require 'users_helper'
 
 class SiteControllerTest < ActionController::TestCase
+  include UsersHelper
   test "should get index" do
     get :index
     assert_response :success
@@ -9,6 +11,12 @@ class SiteControllerTest < ActionController::TestCase
   test "should get a link to index" do
     get :index
     assert_select '#toolbar ul li a.index', 1, 'Link to index in the toolbar not found'
+  end
+
+  test "When LeChuck is logged must have a link to settings into the toolbar" do
+    login users(:LeChuck)
+    get :index
+    assert_select '#toolbar ul li a[href="' + settings_path + '"]', 1, 'Link to settings in the toolbar not found'
   end
 
 end
