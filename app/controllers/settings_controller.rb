f447e30b198ac2ef0162f28a03820fa75732bf15
eltortuganegra <jorge@questionity.com>
2016-04-22 08:46:49 +0200

    When users disable their accounts with invalid confirmation code then an error is shown #121

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -8,5 +8,23 @@ class SettingsController < ApplicationController
   end
 
   def disable_account_confirmation
+    if is_confirmation_code_coming?
+      if ! is_confirmation_code_valid?
+        flash.now[:alert] = 'Confirmation code not found. Please, review your email and verify your confirmation code.'
+      end
+    end
   end
+
+  private
+    def settings_params
+      params.fetch(:settings, {}).permit(:confirmation_code)
+    end
+
+    def is_confirmation_code_valid?
+      false
+    end
+
+    def is_confirmation_code_coming?
+      ! settings_params.nil? && ! settings_params[:confirmation_code].nil?
+    end
 end
